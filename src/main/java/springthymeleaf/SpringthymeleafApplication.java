9834a612d27fff3d34f3705ab0788a4e38ea04ac
joaosinho2 <algamerjoao1@hotmail.com>
2022-08-25 09:43:00 -0300

    projeto criado

---
+++ src/main/java/springthymeleaf/SpringthymeleafApplication.java
@@ -0,0 +1,13 @@
+package springthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringthymeleafApplication.class, args);
+    }
+
+}

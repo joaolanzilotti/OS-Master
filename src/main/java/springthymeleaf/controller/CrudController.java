f3ed5e92e2774e67c9bd3b66c11d36d2da5972bb
joaosinho2 <algamerjoao1@hotmail.com>
2022-08-30 12:01:17 -0300

    v1

--- src/main/java/springthymeleaf/controller/CrudController.java
+++ src/main/java/springthymeleaf/controller/CrudController.java
@@ -1,13 +1,17 @@
 package springthymeleaf.controller;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import springthymeleaf.dto.RequisicaoCliente;
 import springthymeleaf.entities.Cliente;
 import springthymeleaf.repositories.ClienteRepository;
 
@@ -31,6 +35,34 @@ public class CrudController {
     public ModelAndView paginaInicio() {
         ModelAndView mv = new ModelAndView("index");
         return mv;
+    }
+
+    @GetMapping("/cadastro")
+    public String paginaCadastro() {
+        return "cadastro";
+    }
+
+    //Estou Criando um metodo que vai receber minha classe entidade Clientes
+    //neste caso eu chamei no metodo minha classe RequisicaoCliente para proteger os dados! eu poderia chamar diretamente a classe Cliente
+    @PostMapping("/cadastro")
+    public ModelAndView cadastro(@Valid RequisicaoCliente requisicao, BindingResult bidingResult) {
+        //Igualando os dados da classe cliente com a classe requisicao, para proteger os dados!
+        Cliente cliente = requisicao.toCliente();
+
+        System.out.println(requisicao);
+
+        if (bidingResult.hasErrors()) {
+            System.out.println("CPF INVALIDO");
+            ModelAndView mv = new ModelAndView("/cadastro");
+            return mv;
+
+        } else {
+            clienteRepository.save(cliente);
+            return new ModelAndView("redirect:/clientes");
+        }
+        
+
+        //catch(javax.validation.ConstraintViolationException e1){System.out.println("CPF INVALIDO");}
     }
 
 }

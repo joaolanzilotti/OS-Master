ab36a6a5a640acf609a39080751373bdbca29154
joaosinho2 <algamerjoao1@hotmail.com>
2022-09-01 19:13:47 -0300

    nova classe html implementada, detalhes

--- src/main/java/springthymeleaf/controller/CrudController.java
+++ src/main/java/springthymeleaf/controller/CrudController.java
@@ -1,6 +1,7 @@
 package springthymeleaf.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -38,7 +40,7 @@ public class CrudController {
     }
 
     //Para o th:field e o th:object funcionar tem que estanciar o minha classe DTO no meu @getMapping e também no @Post 
-    @GetMapping("cadastro")
+    @GetMapping("/cadastro")
     public String paginaCadastro(RequisicaoCliente r) {
 
         return "cadastro";
@@ -46,7 +48,7 @@ public class CrudController {
 
     //Estou Criando um metodo que vai receber minha classe entidade Clientes
     //neste caso eu chamei no metodo minha classe RequisicaoCliente para proteger os dados! eu poderia chamar diretamente a classe Cliente
-    @PostMapping("/clientes")
+    @PostMapping("/clientes") 
     public ModelAndView cadastro(@Valid RequisicaoCliente requisicao, BindingResult erro) {
         //Igualando os dados da classe cliente com a classe requisicao, para proteger os dados!
         Cliente cliente = requisicao.toCliente();
@@ -59,10 +61,31 @@ public class CrudController {
             clienteRepository.save(cliente);
             return new ModelAndView("redirect:/clientes");
         }
+    }
 
-        
+    //com o @PathVariable estou dizendo que o valor passado no meu parametro vai ser igual ao valor do id no meu GetMapping
+    //Optional -> vai evitar algum erro de exception pois o id selecionado pode também nao existir , entao o optional nao deixa esse erro vir por o ID é opcional!
+    @GetMapping("/clientes/{id}")
+    public ModelAndView detalhes(@PathVariable Long id) {
+        Optional<Cliente> optional = this.clienteRepository.findById(id);
 
-        //catch(javax.validation.ConstraintViolationException e1){System.out.println("CPF INVALIDO");}
+        if (optional.isPresent()) {
+
+            //eu usei optional.get pois eu ja chamei meu objeto cliente la!
+            Cliente cliente = optional.get();
+            
+            ModelAndView mv = new ModelAndView("detalhes");
+            mv.addObject("cliente", cliente);
+            
+            return mv;
+
+        } else {
+
+            System.out.println("ID DE CLIENTE NAO ENCONTRADO!");
+            return new ModelAndView("redirect:/clientes");
+
+        }
+
     }
 
 }

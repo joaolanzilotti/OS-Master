9aee3198bb35860195cbe299803a6a9c83153665
joaosinho2 <algamerjoao1@hotmail.com>
2022-09-02 15:09:47 -0300

    mapeamento resolvido

--- src/main/java/springthymeleaf/controller/CrudController.java
+++ src/main/java/springthymeleaf/controller/CrudController.java
@@ -1,7 +1,6 @@
 package springthymeleaf.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,7 +25,7 @@ public class CrudController {
     public ModelAndView paginaClientes() {
         List<Cliente> clientes = clienteRepository.findAll();
 
-        ModelAndView mv = new ModelAndView("clientes");
+        ModelAndView mv = new ModelAndView("/clientes/index");
         mv.addObject("clientes", clientes);
 
         return mv;
@@ -35,57 +33,36 @@ public class CrudController {
 
     @GetMapping("/inicio")
     public ModelAndView paginaInicio() {
-        ModelAndView mv = new ModelAndView("index");
+        ModelAndView mv = new ModelAndView("home");
         return mv;
     }
 
     //Para o th:field e o th:object funcionar tem que estanciar o minha classe DTO no meu @getMapping e também no @Post 
-    @GetMapping("/cadastro")
+    @GetMapping("/new")
     public String paginaCadastro(RequisicaoCliente r) {
 
-        return "cadastro";
+        return "clientes/new";
     }
 
     //Estou Criando um metodo que vai receber minha classe entidade Clientes
     //neste caso eu chamei no metodo minha classe RequisicaoCliente para proteger os dados! eu poderia chamar diretamente a classe Cliente
-    @PostMapping("/clientes") 
+    @PostMapping("/clientes")
     public ModelAndView cadastro(@Valid RequisicaoCliente requisicao, BindingResult erro) {
         //Igualando os dados da classe cliente com a classe requisicao, para proteger os dados!
         Cliente cliente = requisicao.toCliente();
 
         if (erro.hasErrors()) {
-            ModelAndView mv = new ModelAndView("/cadastro");
+            ModelAndView mv = new ModelAndView("clientes/new");
             return mv;
 
         } else {
             clienteRepository.save(cliente);
             return new ModelAndView("redirect:/clientes");
         }
-    }
 
-    //com o @PathVariable estou dizendo que o valor passado no meu parametro vai ser igual ao valor do id no meu GetMapping
-    //Optional -> vai evitar algum erro de exception pois o id selecionado pode também nao existir , entao o optional nao deixa esse erro vir por o ID é opcional!
-    @GetMapping("/clientes/{id}")
-    public ModelAndView detalhes(@PathVariable Long id) {
-        Optional<Cliente> optional = this.clienteRepository.findById(id);
+        
 
-        if (optional.isPresent()) {
-
-            //eu usei optional.get pois eu ja chamei meu objeto cliente la!
-            Cliente cliente = optional.get();
-            
-            ModelAndView mv = new ModelAndView("detalhes");
-            mv.addObject("cliente", cliente);
-            
-            return mv;
-
-        } else {
-
-            System.out.println("ID DE CLIENTE NAO ENCONTRADO!");
-            return new ModelAndView("redirect:/clientes");
-
-        }
-
+        //catch(javax.validation.ConstraintViolationException e1){System.out.println("CPF INVALIDO");}
     }
 
 }

b658fb1f866113be95dcedda751b8b5a29386207
joaosinho2 <algamerjoao1@hotmail.com>
2022-10-09 23:30:34 -0300

    Update TecnicoRepository.java

--- src/main/java/springthymeleaf/repositories/TecnicoRepository.java
+++ src/main/java/springthymeleaf/repositories/TecnicoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import springthymeleaf.entities.Tecnico;
 
 public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
+
+    Tecnico findByEmailTecnico(String email);
     
 }

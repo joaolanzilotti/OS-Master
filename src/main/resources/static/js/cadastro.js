a74258f60f16b50de4f153b0c29ccba45b80ca5e
joaosinho2 <algamerjoao1@hotmail.com>
2022-10-19 14:18:42 -0300

    implement animation for Buttons and Pages

--- src/main/resources/static/js/cadastro.js
+++ src/main/resources/static/js/cadastro.js
@@ -1,34 +1,5 @@
 // // Example starter JavaScript for disabling form submissions if there are invalid fields,
 
-// API url 
-const api_url =
-"https://mygfgapi.free.beeceptor.com/my/api/path";
-  
-// Defining async function 
-async function getapi(url) {
-  
-    // Storing response 
-    const response = await fetch(url);
-  
-    // Storing data in form of JSON 
-    var apidata = await response.json();
-    console.log(apidata);
-    if (response) {
-        hideSpinner();
-    }
-    document.getElementById("data").innerHTML
-        = `<h1>${apidata.data}</h1>`;
-}
-  
-// Calling that async function 
-getapi(api_url);
-  
-// Function to hide the Spinner
-function hideSpinner() {
-    document.getElementById('spinner')
-            .style.display = 'none';
-} 
-
 // $(document).ready(function () {
 //   $('#tabela').DataTable({
 //     responsive: {

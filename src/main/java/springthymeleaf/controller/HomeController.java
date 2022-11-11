f3b47d5bd74c67e96da9bb5931cc9a851ed78cc5
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-11 13:38:42 -0300

    dashboard in progress...

--- src/main/java/springthymeleaf/controller/HomeController.java
+++ src/main/java/springthymeleaf/controller/HomeController.java
@@ -1,15 +1,54 @@
 package springthymeleaf.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import springthymeleaf.entities.Cliente;
+import springthymeleaf.entities.OrdemServico;
+import springthymeleaf.entities.Produto;
+import springthymeleaf.entities.Servico;
+import springthymeleaf.repositories.ClienteRepository;
+import springthymeleaf.repositories.OrdemServicoRepository;
+import springthymeleaf.repositories.ProdutoRepository;
+import springthymeleaf.repositories.ServicoRepository;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ServicoRepository servicoRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @GetMapping("/inicio")
     public ModelAndView paginaInicio() {
+
+        List<Cliente> clientes = clienteRepository.findAll();
+        List<Servico> servicos = servicoRepository.findAll();
+        List<Produto> produtos = produtoRepository.findAll();
+        List<OrdemServico> ordemServicos = ordemServicoRepository.findAll();
+
+        int contagemCliente = clientes.size();
+        int contagemServico = servicos.size();
+        int contagemProduto = produtos.size();
+        int contagemOrdemServico = ordemServicos.size();
+
         ModelAndView mv = new ModelAndView("index");
+        mv.addObject("contagemCliente", contagemCliente);
+        mv.addObject("contagemServico", contagemServico);
+        mv.addObject("contagemProduto", contagemProduto);
+        mv.addObject("contagemOrdemServico", contagemOrdemServico);
         return mv;
     }
     

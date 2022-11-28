286d609f19f01b693ae1a69b5d85588ab69c37f6
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-28 10:37:19 -0300

    Changes For Class HomeController and Delete Class HomeService

--- src/main/java/springthymeleaf/controller/HomeController.java
+++ src/main/java/springthymeleaf/controller/HomeController.java
@@ -5,26 +5,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import springthymeleaf.services.HomeService;
+import springthymeleaf.services.ClienteService;
+import springthymeleaf.services.OrdemServicoService;
+import springthymeleaf.services.ProdutoService;
+import springthymeleaf.services.ServicoService;
 
 @Controller
 public class HomeController {
 
     @Autowired
-    private HomeService homeService;
+    private ClienteService clienteService;
+
+    @Autowired
+    private ServicoService servicoService;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private OrdemServicoService ordemServicoService;
 
     @GetMapping("/inicio")
     public ModelAndView paginaInicio() {
 
-        int contagemCliente = homeService.findAllClientes().size();
-        int contagemServico = homeService.findAllServicos().size();
-        int contagemProduto = homeService.findAllProdutos().size();
-        int contagemOrdemServico = homeService.findAllOrdensServico().size();
+        int contagemCliente = clienteService.findAllClientes().size();
+        int contagemServico = servicoService.findAllServicos().size();
+        int contagemProduto = produtoService.findAllProdutos().size();
+        int contagemOrdemServico = ordemServicoService.findAllOrdemServico().size();
 
         ModelAndView mv = new ModelAndView("index");
         mv.addObject("contagemCliente", contagemCliente);
         mv.addObject("contagemServico", contagemServico);
-        mv.addObject("contagemProduto", contagemProduto);
+        mv.addObject("contagemProduto", contagemProduto); 
         mv.addObject("contagemOrdemServico", contagemOrdemServico);
         return mv;
     }

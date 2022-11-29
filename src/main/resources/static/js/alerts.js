5c1734b3910b5aada6ff3a48fe04f687ede2bf56
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-11-29 12:18:57 -0300

    changes for alerts.js

---
+++ src/main/resources/static/js/alerts.js
@@ -0,0 +1,38 @@
+var titulo;
+var mensagem;
+
+function alertaCadastro(titulo , mensagem) {
+    iziToast.success({
+        title: titulo,
+        message: mensagem,
+        progressBarColor: 'black',
+
+    });
+};
+
+function alertaEditar(titulo, mensagem) {
+    iziToast.success({
+        title: titulo,
+        message: mensagem,
+        progressBarColor: 'black',
+
+    });
+};
+
+function alertaRemover(titulo, mensagem) {
+    iziToast.warning({
+        title: titulo,
+        titleColor: 'white',
+        message: mensagem,
+        backgroundColor: 'rgba(255,0,0,0.7)',
+        messageColor: 'white',
+        progressBarColor: 'black',
+    });
+};
+
+function alerta(titulo, mensagem){
+    iziToast.warning({
+        title: titulo,
+        message: mensagem,
+    });
+};

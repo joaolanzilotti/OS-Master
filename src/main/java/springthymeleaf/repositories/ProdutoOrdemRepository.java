4ea953093b4c325955a30519474bed33cf962b50
João Pedro Lanzilotti Ferretti <algamerjoao1@hotmail.com>
2022-12-05 12:53:01 -0300

    initialized progress for Query ProdutoOrdem to get products for ordemServico

--- src/main/java/springthymeleaf/repositories/ProdutoOrdemRepository.java
+++ src/main/java/springthymeleaf/repositories/ProdutoOrdemRepository.java
@@ -7,5 +7,22 @@ import springthymeleaf.entities.ProdutoOrdem;
 
 @Repository
 public interface ProdutoOrdemRepository extends JpaRepository<ProdutoOrdem, Long>{
+
+    //@Query("SELECT os FROM OrdemServico os JOIN os.produtoOrdem po")
+    //List<OrdemServico> finddddd();
+
+    //@Query("SELECT po FROM ProdutoOrdem po JOIN po.ordemServico os")
+    //List<ProdutoOrdem> findddddProdutoOrdem();
     
+    /*
+     SELECT 
+p.nome as Nome,
+p.valor as Valor
+from
+produtoordem po
+INNER JOIN ordemservico os ON os.id = po.ordemservico_id
+INNER JOIN produto p ON p.id = po.produto_id
+WHERE po.ordemservico_id = 4;
+     */
+
 }
